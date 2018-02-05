9ac0388b1a7b212839d52a6c3127ef5d624c9510
thatyellowguy91 <lhuang.lhu@gmail.com>
2018-02-05 10:04:13 -0800

    Eyyy

--- Huang Ominetti Filter.py
+++ Huang Ominetti Filter.py
@@ -2,13 +2,12 @@ import PIL
 import matplotlib.pyplot as plt
 import os.path  
 import PIL.ImageDraw
-import Image
 
 directory = os.getcwd()
-image = Image.open(student.jpg).show()
+
 
 def blue_eye(x, y):
     for row in range(x[0], x[1]):
         for column in range(y[0], y[1]):
             if sum(img[row][column])>500:
-                img[row][column]=[0, 0, 257]
+                img[row][column]=[0, 0, 255]
