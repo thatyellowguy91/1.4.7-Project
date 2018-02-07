655ebbd28a68d8f9594ef81b7681435c5f40af0d
thatyellowguy91 <lhuang.lhu@gmail.com>
2018-02-06 23:02:59 -0800

    Fresh Cow Bong

--- Huang Ominetti Filter.py
+++ Huang Ominetti Filter.py
@@ -10,20 +10,12 @@ import matplotlib.pyplot as plt
 import os.path  
 import PIL.ImageDraw
 
-#imports the image
-def blue_eye():
-    directory = os.path.dirname(os.path.abspath(__file__))  
-    img = PIL.Image.open(directory, 'student.jpg')
-    axes.imshow(img)
-    for r in range(589, 1388):
-        for c in range(883,1090):
-            img[r][c][2]=img[r][c][1]
-    fig.show()
-
 def background_change():
+    #locates and imports the image
     filename = input("What is the name of the photo?")
     directory = os.path.dirname(os.path.abspath(__file__))
     filepath = os.path.join(directory, filename)
     img = PIL.Image.open(filepath)
+    #displays the image
     img.show()
     
