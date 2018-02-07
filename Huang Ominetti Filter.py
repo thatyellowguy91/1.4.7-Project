1ae2af2dc527fe6800c79f9cdef1de4f4a3242f3
thatyellowguy91 <lhuang.lhu@gmail.com>
2018-02-06 18:34:20 -0800

    Plez help

--- Huang Ominetti Filter.py
+++ Huang Ominetti Filter.py
@@ -14,11 +14,10 @@ image_list = []
 file_list = []
 
 def blue_eye():
-    top_left_cordinates = input("Please enter the first set of coordinates for the eye locations.")
-    bottom_right_coordinates = input("Please enter the second set of coordinates for the eye location.")
-    for row in range(top_left_coordinates[0], bottom_right_coordinates[0]):
-        for column in range(top_left_coordinates[1], bottom_right_coordinates[1]):
-            if sum(img[row][column])>500:
+    tlc = int(input("Please enter the first set of coordinates for the eye locations."))
+    brc = int(input("Please enter the second set of coordinates for the eye location."))
+    for row in range(tlc[0], brc[0]):
+        for column in range(tlc[1], brc[1]):
                 img[row][column]=[0, 0, 255]
 
-print("Please enter coordinates in list form when prompted.  Enter 'eye_change()' to begin.")
+print("Please enter coordinates in list form when prompted.  Enter 'blue_eye()' to begin.")
