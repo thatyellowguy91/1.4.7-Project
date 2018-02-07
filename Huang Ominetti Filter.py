5ba489c2592e1b153d03d948caeadb14dc5713eb
thatyellowguy91 <lhuang.lhu@gmail.com>
2018-02-06 18:25:38 -0800

    Stafff

--- Huang Ominetti Filter.py
+++ Huang Ominetti Filter.py
@@ -15,10 +15,11 @@ file_list = []
 
 def eye_change():
     color = input('What color would you like your eye?')
-    cordinates = input("Please 
+    top_left_cordinates = input("Please enter the first set of coordinates for the eye locations.")
+    bottom_right_coordinates = input("Please enter the second set of coordinates for the eye location.")
     if color == "Blue" or "blue":
-        for row in range(x[0], x[1]):
-            for column in range(y[0], y[1]):
+        for row in range(top_left_coordinates[0], bottom_right_coordinates[0]):
+            for column in range(top_left_coordinates[1], bottom_right_coordinates[1]):
                 if sum(img[row][column])>500:
                     img[row][column]=[0, 0, 255]
     elif color == "Red" or "red":
@@ -26,3 +27,5 @@ def eye_change():
             for column in range(y[0], y[1]):
                 if sum(img[row][column])>500:
                     img[row][column]=[0, 0, 255]
+
+print("Please enter coordinates in list form when prompted.  Enter 'eye_change()' to begin.")
