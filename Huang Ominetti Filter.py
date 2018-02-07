6dc8cff439a0eb2971e5ba1acc5d94093db88fe3
thatyellowguy91 <lhuang.lhu@gmail.com>
2018-02-06 19:02:07 -0800

    f

--- Huang Ominetti Filter.py
+++ Huang Ominetti Filter.py
@@ -13,14 +13,17 @@ directory = os.path.dirname(os.path.abspath(__file__))
 filename = os.path.join(directory, 'student.jpg')
 student_img = PIL.Image.open(filename)
 fig, axes = plt.subplots(1, 1)
-axes.imshow(student_img, interpolation='none')
-fig.show()
+axes.imshow(student_img)
 
 def blue_eye():
+    directory = os.path.dirname(os.path.abspath(__file__))  
+    filename = os.path.join(directory, 'student.jpg')
+    student_img = PIL.Image.open(filename)
+    fig, axes = plt.subplots(1, 1)
+    axes.imshow(student_img)
     for row in range(50, 50):
         for column in range(50, 50):
-            
-                img[row][column]=[0, 0, 255]
+            img[row][column]=[0, 0, 255]
     fig.show()
 
 blue_eye()
