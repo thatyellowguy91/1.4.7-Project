0ce5f6172b3bd1cceb2bd8e16964a5cd03d42674
thatyellowguy91 <lhuang.lhu@gmail.com>
2018-02-06 23:52:51 -0800

    Bruh

--- Huang Ominetti Filter.py
+++ Huang Ominetti Filter.py
@@ -12,24 +12,20 @@ import os.path
 import PIL.ImageDraw
 import numpy as np
 
-def background_change():
+def paste_item(x,y):
     #locates and imports the image
+    #prompts decide which is base image and which is pasted
     filename = input("What is the name of the photo?")
     directory = os.path.dirname(os.path.abspath(__file__))
     filepath = os.path.join(directory, filename)
     img = plt.imread(filepath)
-    fig, ax = plt.subplots(1, 1)
-    height = len(img)
-    width = len(img[0])
-<<<<<<< HEAD
-    for row in range(604,1351):
-        for column in range(883,1046):
-            img[row][column][0,255,255]
-=======
-    for r in range(604,1351):
-        for c in range(883,1046):
-            img[r][c][2]
->>>>>>> parent of 6c0d574... Bob's big burgers
+    #locates and imports a second image
+    filename2 = input("What is the name of the image to paste on top?")
+    directory2 = os.path.dirname(os.path.abspath(__file__))
+    filepath2 = os.path.join(directory2, filename2)
+    img2 = plt.imread(filepath2)
+    #pastes 2nd image onto the first
+    img.paste(img2,(x,y), mask = img2)
     #displays the image
     ax.imshow(img, interpolation = 'none')
     fig.show()
