e568dd340fc2004a89ad101d5cb948975323ebc0
thatyellowguy91 <lhuang.lhu@gmail.com>
2018-02-06 22:13:29 -0800

    Uhhh Plz work

--- Huang Ominetti Filter.py
+++ Huang Ominetti Filter.py
@@ -12,14 +12,13 @@ import PIL.ImageDraw
 
 #imports the image
 def blue_eye():
+    isPLT = input("Is the image a PLT image?")
     directory = os.path.dirname(os.path.abspath(__file__))  
     filename = os.path.join(directory, 'student.jpg')
-    if isPLT:
+    if isPLT == "True":
         img = PIL.Image.open(filename)
     else:
-        img = PIL.Image.open(filepath)
-    fig, axes = plt.subplots(1, 1)
-    axes.imshow(img)
+        img = PIL.Image.open(directory)
     for row in range(589, 1388):
         for column in range(883,1090):
             img[row][column]=[0, 0, 255]
