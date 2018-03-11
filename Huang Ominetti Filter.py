86e3d45d8f35f2bbc6a016d81a19bfe86cec7301
thatyellowguy91 <lhuang.lhu@gmail.com>
2018-03-10 21:58:43 -0800

    juss stuuffffs

--- Huang Ominetti Filter.py
+++ Huang Ominetti Filter.py
@@ -1,31 +1,19 @@
-# -*- coding: utf-8 -*-
 '''Eye Color Filter
 
     by Lawrence Huang and Christian Ominetti
     
-                                            '''
-        
+    '''
 #importing necessary libraries
 import PIL
 import matplotlib.pyplot as plt
 import os.path  
 import PIL.ImageDraw
-import numpy as np
 
-def paste_item(x,y):
-    #locates and imports the image
-    #prompts decide which is base image and which is pasted
-    filename = input("What is the name of the photo?")
-    directory = os.path.dirname(os.path.abspath(__file__))
-    filepath = os.path.join(directory, filename)
-    img = plt.imread(filepath)
-    #locates and imports a second image
-    filename2 = input("What is the name of the image to paste on top?")
-    directory2 = os.path.dirname(os.path.abspath(__file__))
-    filepath2 = os.path.join(directory2, filename2)
-    img2 = plt.imread(filepath2)
-    #pastes 2nd image onto the first
-    img.paste(img2,(x,y), mask = img2)
-    #displays the image
-    ax.imshow(img, interpolation = 'none')
-    fig.show()
+image = PIL.Image.open(directory)
+directory = os.getcwd()
+
+def blue_eye(x, y):
+    for row in range(x[0], x[1]):
+        for column in range(y[0], y[1]):
+            if sum(img[row][column])>500:
+                img[row][column]=[0, 0, 255]
