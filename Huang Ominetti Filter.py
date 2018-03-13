fa9ecf37088fb41ba267d466aa8adc3b7895f951
thatyellowguy91 <lhuang.lhu@gmail.com>
2018-03-13 15:50:37 -0700

    Idek who Megan is

--- Huang Ominetti Filter.py
+++ Huang Ominetti Filter.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-'''Eye Color Filter
+'''
+    Eye Color Filter
     by Lawrence Huang and Christian Ominetti
     
                                             '''
@@ -8,15 +9,15 @@ import PIL
 import matplotlib.pyplot as plt
 import os.path  
 import PIL.ImageDraw
+import numpy as np
 
-directory = os.getcwd()
-imgarray = PIL.image.open('student.jpg')
+directory = os.path.dirname(os.path.abspath(__file__))
+filepath = os.path.join(directory, 'student.jpg')
+fig = PIL.Image.open(filepath)
 
 def blue_eye(x, y):
     for row in range(x[0], x[1]):
         for column in range(y[0], y[1]):
-            if sum(img[row][column])>500:
-                img[row][column]=[0, 0, 255]
-    fig, ax = plt.subplots(1, 1)
+            if sum(fig[row][column]):
+                fig[row][column]=[0, 0, 255]
     fig.show()
-
