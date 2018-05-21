ca7944668ff076d8ca749d1cbdfc061c1658a04b
Cominetti <33034319+Cominetti@users.noreply.github.com>
2018-05-20 22:27:03 -0700

    the OG

--- Huang Ominetti Filter.py
+++ Huang Ominetti Filter.py
@@ -1,10 +1,6 @@
 # -*- coding: utf-8 -*-
-'''
-    Eye Color Filter
-    by Lawrence Huang and Christian Ominetti
-    
-                                            '''
 '''Eye Color Filter
+
     by Lawrence Huang and Christian Ominetti
     
                                             '''
@@ -16,40 +12,13 @@ import os.path
 import PIL.ImageDraw
 import numpy as np
 
-directory = os.path.dirname(os.path.abspath(__file__))
-filepath = os.path.join(directory, 'student.jpg')
-fig = PIL.Image.open(filepath)
-
-def blue_eye(x, y):
-    for row in range(x[0], x[1]):
-        for column in range(y[0], y[1]):
-
-         if sum(fig[row][column]):
-                fig[row][column]=[0, 0, 255]
-    fig.show()
-
-def background_change():
-    #locates and imports the image
-
- def paste_item(x,y):
+def paste_item(x,y):
     #locates and imports the image
     #prompts decide which is base image and which is pasted
-
     filename = input("What is the name of the photo?")
     directory = os.path.dirname(os.path.abspath(__file__))
     filepath = os.path.join(directory, filename)
     img = plt.imread(filepath)
-
-    fig, ax = plt.subplots(1, 1)
-    height = len(img)
-    width = len(img[0])
-    for r in range(604,1351):
-        for c in range(883,1046):
-            img[r][c][2]
-    #displays the image
-    ax.imshow(img, interpolation = 'none')
-    fig.show()
-
     #locates and imports a second image
     filename2 = input("What is the name of the image to paste on top?")
     directory2 = os.path.dirname(os.path.abspath(__file__))
@@ -60,7 +29,3 @@ def background_change():
     #displays the image
     ax.imshow(img, interpolation = 'none')
     fig.show()
-
-            if sum(img[row][column])>500:
-                img[row][column]=[0, 0, 255]
-    img.show()
