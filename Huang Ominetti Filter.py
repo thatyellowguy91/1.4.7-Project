502f2fac415d9f0f79dc0a7d5701bad7da66e9ff
Cominetti <33034319+Cominetti@users.noreply.github.com>
2018-05-20 21:55:05 -0700

    heck

--- Huang Ominetti Filter.py
+++ Huang Ominetti Filter.py
@@ -1,18 +1,14 @@
 # -*- coding: utf-8 -*-
-<<<<<<< HEAD
 '''
     Eye Color Filter
     by Lawrence Huang and Christian Ominetti
     
                                             '''
-=======
 '''Eye Color Filter
-
     by Lawrence Huang and Christian Ominetti
     
                                             '''
         
->>>>>>> parent of 86e3d45... juss stuuffffs
 #importing necessary libraries
 import PIL
 import matplotlib.pyplot as plt
@@ -20,32 +16,30 @@ import os.path
 import PIL.ImageDraw
 import numpy as np
 
-<<<<<<< HEAD
 directory = os.path.dirname(os.path.abspath(__file__))
 filepath = os.path.join(directory, 'student.jpg')
 fig = PIL.Image.open(filepath)
 
-<<<<<<< HEAD
 def blue_eye(x, y):
     for row in range(x[0], x[1]):
         for column in range(y[0], y[1]):
-<<<<<<< HEAD
-            if sum(fig[row][column]):
+
+         if sum(fig[row][column]):
                 fig[row][column]=[0, 0, 255]
     fig.show()
-=======
+
 def background_change():
     #locates and imports the image
-=======
-def paste_item(x,y):
+
+ def paste_item(x,y):
     #locates and imports the image
     #prompts decide which is base image and which is pasted
->>>>>>> parent of 86e3d45... juss stuuffffs
+
     filename = input("What is the name of the photo?")
     directory = os.path.dirname(os.path.abspath(__file__))
     filepath = os.path.join(directory, filename)
     img = plt.imread(filepath)
-<<<<<<< HEAD
+
     fig, ax = plt.subplots(1, 1)
     height = len(img)
     width = len(img[0])
@@ -55,8 +49,7 @@ def paste_item(x,y):
     #displays the image
     ax.imshow(img, interpolation = 'none')
     fig.show()
->>>>>>> parent of 6c0d574... Bob's big burgers
-=======
+
     #locates and imports a second image
     filename2 = input("What is the name of the image to paste on top?")
     directory2 = os.path.dirname(os.path.abspath(__file__))
@@ -67,9 +60,7 @@ def paste_item(x,y):
     #displays the image
     ax.imshow(img, interpolation = 'none')
     fig.show()
->>>>>>> parent of 86e3d45... juss stuuffffs
-=======
+
             if sum(img[row][column])>500:
                 img[row][column]=[0, 0, 255]
     img.show()
->>>>>>> parent of 9336c4d... This Kinda Works!
