22f4de71a5f415ee5d62014266bc32ba4a55ecba
Cominetti <33034319+Cominetti@users.noreply.github.com>
2018-05-21 00:55:01 -0700

    heck

--- Huang Ominetti Filter.py
+++ Huang Ominetti Filter.py
@@ -12,22 +12,22 @@ import os.path
 import PIL.ImageDraw
 import numpy as np
 
-def colour(orig):
-    i = orig #set variable for image
-    directory = os.path.dirname(os.path.abspath(__file__)) #set path of directory
-    File = os.path.join(directory, i) #set the directory to a specific file
-    img = plt.imread(File) #allow the file to be opened in plt
-    fig, ax = plt.subplots(1, 1) #create subplots
-    height = len(img) #set height variable to y values of image
-    width = len(img[0]) #set width variable x values of image
-    for r in range(height): 	#set up nested loop
+def no_color(orig):
+    i = orig #sets the variable for the selected image
+    directory = os.path.dirname(os.path.abspath(__file__)) #selects path of directory
+    File = os.path.join(directory, i) #selects a specific file as the directory
+    img = plt.imread(File) #enables the file to be opened as a plt
+    fig, ax = plt.subplots(1, 1) #creates subplots
+    height = len(img) #matches the height to the y value of the image 
+    width = len(img[0]) #matches the width to the x value of the image
+    for r in range(height): 	#sets up the nested loop
         for c in range(width):
-            if sum(img[r][c])<100: #test for if sum of RGB values in selected area is less than 100
+            if sum(img[r][c])<100: #scans RGB values to determine if the sum is less than or greater than 100 within a given area
                 img[r][c] = [0,0,255]
-            img[r][c]=[sum(img[r][c])/3] #takes the sum of the RGB values in the selected area, divided by three
+            img[r][c]=[sum(img[r][c])/3] #divides the sum of the RGB values in the givin area by 3
     ax.set_axis_off() #hides the axes			
     ax.imshow(img, interpolation='none')
-    fig.savefig('temp.jpg') #save as .jpg file
+    fig.savefig('temp.jpg') #saves the file as a .jpg file
     result = os.path.join(directory, 'temp.jpg') #sets the new file as result
-    img = PIL.Image.open(result) #opens the image using PIL
-    img.show() #allows file to be saved into folder
+    img = PIL.Image.open(result) #uses PIL to open the image
+    img.show() #enables the file to be saved in folder
