4cc653748b9f5d9ea242f88ba29b2efb6586749d
thatyellowguy91 <lhuang.lhu@gmail.com>
2018-05-21 00:04:47 -0700

    a lotz of stuffz

--- Huang Ominetti Filter.py
+++ Huang Ominetti Filter.py
@@ -16,7 +16,25 @@ def paste_item(firstimg):
     directory = os.path.dirname(os.path.abspath(__file__))
     otherfile = os.path.join(directory, 'earth.png') #selects the file
     img = PIL.Image.open(otherfile) #opens new image
-    img.resize((30,30)) #resize new image
     firstimg.paste(img,(100,100)) #paste newimage at (100,100) on original image
-    final=firstimg.rotate(360)
-    final.show()
+    return firstimg
+
+def def color(orig):
+    i = image #set variable for image
+    directory = os.path.dirname(os.path.abspath(__file__)) #set path of directory
+    File = os.path.join(directory, i) #set the directory to a specific file
+    img = plt.imread(File) #allow the file to be opened in plt
+    fig, ax = plt.subplots(1, 1) #create subplots
+    height = len(img) #set height variable to y values of image
+    width = len(img[0]) #set width variable x values of image
+    for r in range(height): 	#set up nested loop
+        for c in range(width):
+            if sum(img[r][c])<100: #test for if sum of RGB values in selected area is less than 100
+                img[r][c] = [20,0,0]
+            img[r][c]=[sum(img[r][c])/3] #takes the sum of the RGB values in the selected area, divided by three
+    ax.set_axis_off() #hides the axes			
+    ax.imshow(img, interpolation='none')
+    fig.savefig('temp.jpg') #save as .jpg file
+    result = os.path.join(directory, 'temp.jpg') #sets the new file as result
+    imag = PIL.Image.open(result) #opens the image using PIL
+    return imag #allows file to be saved into folder
