6f305d65301097c1ceb6abe05a34f1303e0f7db4
thatyellowguy91 <lhuang.lhu@gmail.com>
2018-05-20 23:15:19 -0700

    sumwut ok

--- Huang Ominetti Filter.py
+++ Huang Ominetti Filter.py
@@ -17,5 +17,5 @@ def paste_item(firstimg):
     otherfile = os.path.join(directory, 'student.jpg') #selects the file
     img = PIL.Image.open(otherfile) #opens new image
     img.resize((30,30)) #resize new image
-    firstimg.paste(img, (100,100)) #paste newimage at (100,100) on original image
+    firstimg.paste(img,(100,100)) #paste newimage at (100,100) on original image
     return firstimg
